ac5b037d94175509d7e8278bb8adb10af33a1d61
박정훈 <jhpark@daou.co.kr>
2022-01-06 15:18:08 +0900

    Initial commit

---
+++ src/main/java/com/gsheet/gsheetsender/GsheetSenderApplication.java
@@ -0,0 +1,13 @@
+package com.gsheet.gsheetsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsheetSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsheetSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsheet/gsheetsender/GsheetSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsheet.gsheetsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsheetSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
